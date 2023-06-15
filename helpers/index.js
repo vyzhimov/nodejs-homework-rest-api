d129b470fec2ab7001106c4bfdcc0d65af47df7e
Alexander Vyzhymov <vizimov@gmail.com>
2023-06-15 20:11:03 +0300

    fixed validate export

--- helpers/index.js
+++ helpers/index.js
@@ -1,9 +1,7 @@
 const HttpError = require("./HttpError");
-const dataValidator = require("./dataValidator");
 const ctrlWrapper = require("./ctrlWrapper");
 
 module.exports = {
   HttpError,
-  dataValidator,
   ctrlWrapper,
 };

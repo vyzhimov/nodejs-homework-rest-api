a3108d822560c0dfb7554e7c0af7c198dca5e00e
Alexander Vyzhymov <vizimov@gmail.com>
2023-07-02 11:04:37 +0300

    fixed-errors

--- controllers/users.js
+++ controllers/users.js
@@ -68,11 +68,10 @@ const getCurrent = async (req, res) => {
 
 const logout = async (req, res) => {
   const { _id } = req.user;
+
   await User.findByIdAndUpdate(_id, { token: "" });
 
-  res.status(204).json({
-    message: "No content",
-  });
+  res.status(204).json();
 };
 
 const updateSubscription = async (req, res) => {
